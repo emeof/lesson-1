e6791a40dbc41a1d6a7e051688a708a426d29469
emeof <emeof489@gmail.com>
2022-12-09 18:55:48 -0600

    done i guess

---
+++ Lesson 1/Randomplaceiguess.java
@@ -0,0 +1,4 @@
+public class Randomplaceiguess {
+    public static void main(String[] args){
+    }
+}
--- Lesson 1/src/ProblemOne.java
+++ Lesson 1/src/ProblemOne.java
@@ -3,5 +3,6 @@ public class ProblemOne {
     public static void main(String[] args){
        System.out.println("yo wahts up johnathon");
         System.out.println("9+10=21");
+    
     }
 }
